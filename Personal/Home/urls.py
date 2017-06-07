1b0e71b1d491a8333f9a5b93b87cc619d2947d8a
Ngahu <jamaalaraheem@gmail.com>
2017-06-08 00:42:59 +0300

    this contains creating and mapping of the class based view into returning a test in the browser it also contains the creating of the css and js folders

--- Personal/Home/urls.py
+++ Personal/Home/urls.py
@@ -1,1 +1,14 @@
 from django.conf.urls import url
+
+from Home.views import HomePage
+
+
+
+
+urlpatterns = [
+    url(r'^$',HomePage.as_view()),
+    #url(r'^create/', post_create, name='create'),
+    #url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    #url(r'^update/', post_update, name='update'),
+    #url(r'^delete/', post_delete, name='delete'),
+]
--- Personal/Home/views.py
+++ Personal/Home/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.template import loader
 
-# Create your views here.
+
+class HomePage(View):
+    """
+    this views returns the basic homepage the home of the webpage
+    """
+    def get(self,request):
+        template = loader.get_template('Home/index.html')
+        return HttpResponse(template.render(request))
+        #return HttpResponse("hello Champ")
